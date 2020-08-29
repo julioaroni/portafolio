546fd64af2a4709761d60185b60e1eede03ce109
Julio Aroni <jc_aroni@hotmail.com>
2020-08-29 12:37:22 -0500

    listo para producción

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InfoPaginaService {
+  info: InfoPagina = {};
+  cargada = false;
+
+  equipo: any[] = [];
+
+  constructor(private http: HttpClient) {
+    // console.log('Servicio de infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
+    //Leer el archivo JSON
+    this.http
+      .get('assets/data/data-pagina.json')
+      .subscribe((resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+        // console.log(resp);
+        //console.log(resp['email']);
+      });
+  }
+
+  private cargarEquipo() {
+    //Leer el archivo JSON
+    this.http
+      .get('https://angular-html-92cfb.firebaseio.com/equipo.json')
+      .subscribe((resp: any) => {
+        this.equipo = resp;
+        // console.log(resp);
+        //console.log(resp['email']);
+      });
+  }
+}
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from '../../services/info-pagina.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(public _servicio: InfoPaginaService, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  buscarProducto(termino: string) {
+    if (termino.length < 1) {
+      return;
+    }
+    this.router.navigate(['/search', termino]);
+    // console.log(termino);
   }
-
 }
